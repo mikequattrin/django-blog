8e1d406607926ed134a75302f9024f6d986992b4
mquattrin@sapient.com <mikequattrin@gmail.com>
2016-02-08 16:09:56 -0500

    Fixed article page. Some cleanup required

--- articles/views.py
+++ articles/views.py
@@ -5,12 +5,10 @@ from django.http import Http404
 from .models import Article
 
 def articles(request):
-    article_random = Article.objects.order_by('?').first()
     articles_random = Article.objects.order_by('?')[:4]
     article_list = Article.objects.order_by('article_publication_date')[:3]
     return render(request, 'articles/articles.html', {
         'article_list': article_list, 
-        'article_random': article_random,
         'articles_random': articles_random,
         })
 
