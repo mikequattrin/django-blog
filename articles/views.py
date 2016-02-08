83d32020496c80343b8adec08d04ac8b2c8dadff
mquattrin@sapient.com <mikequattrin@gmail.com>
2016-02-08 13:54:32 -0500

    Some changes to the articles page. Random primary article. Listed 3 latest articles.

--- articles/views.py
+++ articles/views.py
@@ -5,8 +5,12 @@ from django.http import Http404
 from .models import Article
 
 def articles(request):
-    article_list = Article.objects.order_by('-article_publication_date')[:5]
-    return render(request, 'articles/articles.html', {'article_list': article_list})
+    article_random = Article.objects.order_by('?').first()
+    article_list = Article.objects.order_by('-article_publication_date')[:3]
+    return render(request, 'articles/articles.html', {
+        'article_list': article_list, 
+        'article_random': article_random,
+        })
 
 def article(request, Article_id):
     try:
