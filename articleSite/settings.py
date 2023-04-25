a00756497c11b00d751df5bb7a385ef324bf2ba0
Mike Quattrin <mikequattrin@gmail.com>
2023-04-25 19:18:43 -0400

    Some notes added

--- articleSite/settings.py
+++ articleSite/settings.py
@@ -104,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
+# ToDo
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-ca'
@@ -118,6 +119,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript)
+# Static file paths
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
